2981304cd1872716de24147e424b1d6bcd2aa5fa
matthias springstein <matthias.springstein@gmail.com>
2025-09-13 12:12:53 +0200

    Remove a console log from search.js and fix checkPost timer.

--- frontend_vue3/src/stores/search.js
+++ frontend_vue3/src/stores/search.js
@@ -20,31 +20,28 @@ export const useSearchStore = defineStore('search', () => {
 
     params = mergedParams
     const body = { ...mergedParams, filters }
-    console.log(body)
-    
-    axios.post('/search', { params: body })
-      .then(({ data }) => {
-        if (data.job_id !== undefined) {
-          jobId.value = data.job_id
-          setTimeout(() => checkLoad, 500)
-        } else {
-          entries.value = data.entries
-          aggregations.value = data.aggregations
-        }
-      })
+
+    axios.post('/search', { params: body }).then(({ data }) => {
+      if (data.job_id !== undefined) {
+        jobId.value = data.job_id
+        setTimeout(() => checkPost(), 500)
+      } else {
+        entries.value = data.entries
+        aggregations.value = data.aggregations
+      }
+    })
   }
 
   function checkPost() {
     const body = { job_id: jobId.value }
-    axios.post('/search', { params: body })
-      .then(({ data }) => {
-        if (data.job_id !== undefined) {
-          jobId.value = data.job_id
-          setTimeout(() => checkLoad, 500)
-        } else {
-          entries.value = data.entries
-          aggregations.value = data.aggregations
-        }
+    axios.post('/search', { params: body }).then(({ data }) => {
+      if (data.job_id !== undefined) {
+        jobId.value = data.job_id
+        setTimeout(() => checkPost(), 500)
+      } else {
+        entries.value = data.entries
+        aggregations.value = data.aggregations
+      }
     })
   }
 
@@ -60,7 +57,8 @@ export const useSearchStore = defineStore('search', () => {
     entries,
     aggregations,
     post,
+    checkPost,
     setFilters,
-    removeFilters
+    removeFilters,
   }
 })
